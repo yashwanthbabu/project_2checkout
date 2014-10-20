d1aa6e620362fe4562e09c129c88c18ce9c0e9a4
yashwanthbabu <yashwanthbabu.gujarathi@gmail.com>
2014-10-20 15:39:52 +0530

    Adding forms

--- product/forms.py
+++ product/forms.py
@@ -1,1 +1,14 @@
-forms.py
+from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.models import User
+
+from .models import Product, ProductShippingOption
+
+class ProductForm(ModelForm):
+	class Meta:
+		model = Product
+
+
+class ProductShippingOptionForm(ModelForm):
+	class Meta:
+		model = ProductShippingOption
