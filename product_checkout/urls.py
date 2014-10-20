85105502ab761791ddc0a12310130b8bbac2a25b
yashwanthbabu <yashwanthbabu.gujarathi@gmail.com>
2014-10-20 16:30:03 +0530

    adding urls, adding template of payment_success

--- product_checkout/urls.py
+++ product_checkout/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('product.views',
     # Examples:
-    url(r'^store/$', 'product', name="product"),
-    url(r'^store/shipping/', 'shipping', name="productshippingoption"),
+    url(r'^product/$', 'product', name="product"),
+    url(r'^product/shipping/', 'shipping', name="productshippingoption"),
+    url(r'^product/payment/', 'creditcarddetails', name="creditcarddetails"),
+    url(r'^product/paymentsuccess/', TemplateView.as_view(template_name="payment_success.html"), name="success"),
     # url(r'^$', 'product_checkout.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
