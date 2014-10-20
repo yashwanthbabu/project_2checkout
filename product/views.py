e275d22b7620fdf450d85bf93d054a9770254122
yashwanthbabu <yashwanthbabu.gujarathi@gmail.com>
2014-10-20 15:42:31 +0530

    Adding functionality in views, Adding urls

--- product/views.py
+++ product/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, \
+    HttpResponseRedirect
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+from .models import Product, ProductShippingOption
+from .forms import ProductForm, ProductShippingOptionForm
+
+def product(request):
+	form = ProductForm()
+	if request.method == 'POST':
+		form = ProductForm(request.POST)
+		if form.is_valid():
+			form = form.save(commit=False)
+			form.save()
+			return HttpResponseRedirect(reverse('productshippingoption'))
+	return render_to_response("product.html",{'form': form}, context_instance=RequestContext(request))
+
+
+def shipping(request):
+	form = ProductShippingOptionForm()
+	if request.method == "POST":
+		form = ProductShippingOptionForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('product'))
+	return render_to_response("shipping.html", {'form': form}, context_instance=RequestContext(request))
--- product_checkout/urls.py
+++ product_checkout/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('product.views',
     # Examples:
+    url(r'^store/$', 'product', name="product"),
+    url(r'^store/shipping/', 'shipping', name="productshippingoption"),
     # url(r'^$', 'product_checkout.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
