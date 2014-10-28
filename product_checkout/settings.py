97d194e0b9e6a6fd44fc61aa89c13b148f998ab2
yashwanthbabu <yashwanthbabu.gujarathi@gmail.com>
2014-10-28 16:11:11 +0530

    Adding django_extensions to installed apps

--- product_checkout/settings.py
+++ product_checkout/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = (
     'south',
     'localflavor',
     'tocheckout.tocheckout',
+    'django_extensions',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,6 +59,17 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.request",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages"
+    )
 
 ROOT_URLCONF = 'product_checkout.urls'
 
