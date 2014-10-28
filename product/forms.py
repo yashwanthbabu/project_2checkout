840f81713310c95b6ea7888e9f929cf32f017821
yashwanthbabu <yashwanthbabu.gujarathi@gmail.com>
2014-10-28 16:03:02 +0530

    Excluding merchant

--- product/forms.py
+++ product/forms.py
@@ -7,13 +7,16 @@ from .models import Product, ProductShippingOption, CreditCardDetails
 class ProductForm(ModelForm):
 	class Meta:
 		model = Product
+		exclude = ('merchant', )
 
 
 class ProductShippingOptionForm(ModelForm):
 	class Meta:
 		model = ProductShippingOption
+		exclude = ('merchant', 'product', )
 
 
 class CreditCardDetailsForm(ModelForm):
 	class Meta:
 		model = CreditCardDetails
+		exclude = ('merchant', )
